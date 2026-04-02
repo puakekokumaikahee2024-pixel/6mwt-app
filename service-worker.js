fa1a553db903687b7a5bac60a7740fc6e1fecd70
puakekokumaikahee2024-pixel <puakeko.kumaikahee2024@gmail.com>
2026-04-02 21:01:28 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = '6mwt-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './generator.html',
+  './manifest.json',
+  './icon_192.png',
+  './icon_512.png'
+];
+
+// インストール時にキャッシュ
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// 古いキャッシュを削除
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// オフライン時はキャッシュから返す
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).catch(() => caches.match('./index.html'));
+    })
+  );
+});
